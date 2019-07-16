3e4097f0687c9933f867e6423b787a8ecad4711c
Joe Heffernan <jheffheffer@gmail.com>
2019-07-15 20:15:55 -0500

    Changed clinical parse cli to very basic.

--- mcw_readers/cli/mcw_parse_clinical_files.py
+++ mcw_readers/cli/mcw_parse_clinical_files.py
@@ -1,7 +1,5 @@
 import os
-import re
 import sys
-import json
 import openpyxl
 
 import pandas as pd
@@ -9,72 +7,49 @@ import PySimpleGUI as sg
 
 from openpyxl.utils.exceptions import InvalidFileException
 
-VARIABLE_TABLE_FILE = os.path.join('data', 'variable_table.json')
-ALL_VARIABLES_FILE = os.path.join('data', 'all_variables.tsv')
-GARBAGE = re.compile("^=|^raw$|^val$|^\[ERR\]$|^SS$")
-
-def parse_neuroscore(variable_locs, file_path):
-    """Parses variables from variable_locs in excel file file_path"""
-    results = {'variable': [],
-               'value': []}
-
-    wb = openpyxl.load_workbook(file_path)
-    for sheet_name, variables in variable_locs.items():
-        sheet = wb[sheet_name]
-        for variable, coordinate in variables.items():
-            print('Reading variable {} at coordinate {}'.format(variable, coordinate))
-            results['variable'].append(variable)
-            value = sheet[coordinate].value
-            if value is None or GARBAGE.match(value):
-                value = ''
-            results['value'].append(value)
-
-    return results
-
 def main():
     layout = [
         [sg.Text('Id', size=(10, 1), auto_size_text=False, justification='right'),
-         sg.InputText(key='Participant')],
-        [sg.Text('Source File', size=(10, 1), auto_size_text=False, justification='right'), 
-         sg.InputText(key='InFile'), 
+         sg.InputText(key='participant')],
+        [sg.Text('Neuroscore runs', size=(10, 1), auto_size_text=False, justification='right'),
+         sg.InputText(key='runs', default_text='1')],
+        [sg.Text('Neuroscore file', size=(10, 1), auto_size_text=False, justification='right'), 
+         sg.InputText(key='neuroscore_file'), 
          sg.FileBrowse()],
+        [sg.Text('Neuroreader files:', size=(10, 1), auto_size_text=False, justification='right'), 
+         sg.InputText(key='neuroreader_files'), 
+         sg.FilesBrowse()],
         [sg.Submit(), sg.Cancel()]
     ]
     event, values = sg.Window('Neuroparser', layout, default_element_size=(40, 1)).Read()
 
-    if event == "Submit":
-        participant = values["Participant"]
-        in_file = values["InFile"]
+    window = sg.Window('Clinical Parse', layout, default_element_size=(40, 1))
 
-        with open(VARIABLE_TABLE_FILE) as variable_table_json:
-            variable_table = json.load(variable_table_json)
-        all_variables = pd.read_csv(ALL_VARIABLES_FILE, sep='\t')
+    while True:
+        event, values = window.Read()
+        if event == 'Submit':
+            participant = values['participant']
+            runs = values['runs']
+            neuroscore_file = values['neuroscore_file']
+            neuroreader_files = values['neuroreader_files']
+            sg.Popup('Participant      : {}'
+                     'Runs             : {}'
+                     'Neuroscore file  : {}'
+                     'Neuroreader files: {}'.format(participant,
+                                                    runs,
+                                                    neuroscore_file,
+                                                    neuroreader_Files))
+            break
 
-        results = parse_neuroscore(variable_table, in_file)
-        df = pd.DataFrame(results)
-        out_data = (pd.merge(all_variables, df, how='left', on='variable')
-                      .fillna({'value': ''})
-                      .sort_values('number')
-                      [['variable', 'value']])
-        out_data['index'] = 0
-        same_index = list(out_data.variable)
-        out_data = (pd.pivot(out_data, index='index', 
-                             columns='variable', values='value')
-                      .reindex(same_index, axis=1))
-
-        out_path = os.path.splitext(in_file)[0] + '.csv'
-        out_data.to_csv(out_path)
+    window.Close()
 
 if __name__ == '__main__':
     try:
         main()
     except FileNotFoundError as not_found:
-        layout = [[sg.Text("File not found {}".format(not_found.filename), 
-                   font=("Helvetica", 30))]]
-        sg.Window('Neuroparser', layout).Read()
+        sg.PopupError('File not found {}'.format(not_found.filename))
     except InvalidFileException as err:
-        layout = [[sg.Text("{}".format(err), font=("Helvetica", 18))]]
-        sg.Window('Neuroparser', layout).Read()
+        sg.PopupError('{}'.format(err))
     except Exception as err:
-        layout = [[sg.Text("Cannot handle error: {}".format(err), font=("Helvetica", 18))]]
-        sg.Window('Neuroparser', layout).Read()
+        sg.PopupError('Unhandled error: {}\n'
+                      'Email help: jheffernan@mcw.edu'.format(err))
