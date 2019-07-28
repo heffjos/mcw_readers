8c814e41c8044613061f71bc6392eef39e89580c
Joe Heffernan <heffjos@umich.edu>
2019-07-27 23:50:32 -0500

    Changed the GUI design for the neurscore parser. The timepoints can be labeled in neurscore and an optional neuroreader file can be assigned to the timepoint.

--- mcw_readers/cli/mcw_parse_clinical_files.py
+++ mcw_readers/cli/mcw_parse_clinical_files.py
@@ -9,22 +9,33 @@ from openpyxl.utils.exceptions import InvalidFileException
 
 def main():
     layout = [
-        [sg.Text('Id', size=(20, 1), auto_size_text=False, justification='right'),
+        [sg.Text('Id', size=(15, 1), auto_size_text=False, justification='right'),
          sg.InputText(key='participant')],
-        [sg.Text('Neuroscore runs', size=(20, 1), auto_size_text=False, justification='right'),
-         sg.InputText(key='runs', default_text='1')],
-        [sg.Text('Neuroscore file', size=(20, 1), auto_size_text=False, justification='right'), 
+        [sg.Text('Neuroscore tp 1', size=(15, 1), auto_size_text=False, justification='right'),
+         sg.Input(key='tp1', default_text='', size=(6, None), enable_events=True),
+         sg.Text('Neuroreader tp 1'),
+         sg.InputText(key='neuroreader1'),
+         sg.FileBrowse()],
+        [sg.Text('Neuroscore tp 2', size=(15, 1), auto_size_text=False, justification='right'),
+         sg.InputText(key='tp2', default_text='', size=(6, None), enable_events=True),
+         sg.Text('Neuroreader tp 2'),
+         sg.InputText(key='neuroreader2'),
+         sg.FileBrowse()],
+        [sg.Text('Neuroscore tp 3', size=(15, 1), auto_size_text=False, justification='right'),
+         sg.InputText(key='tp3', default_text='', size=(6, None), enable_events=True),
+         sg.Text('Neuroreader tp 3'),
+         sg.InputText(key='neuroreader3'),
+         sg.FileBrowse()],
+        [sg.Text('Neuroscore file', size=(15, 1), auto_size_text=False, justification='right'), 
          sg.InputText(key='neuroscore_file'), 
          sg.FileBrowse()],
-        [sg.Text('Neuroreader files:', size=(20, 1), auto_size_text=False, justification='right'), 
-         sg.InputText(key='neuroreader_files'), 
-         sg.FilesBrowse()],
         [sg.Submit(), sg.Cancel()]
     ]
     window = sg.Window('Clinical Parse', layout, default_element_size=(40, 1))
 
     while True:
         event, values = window.Read()
+        print('{}'.format(event))
         if event == 'Submit':
             participant = values['participant']
             runs = values['runs']
@@ -41,6 +52,10 @@ def main():
             break
         if event is None or event == 'Cancel':
             break
+        for tp in ['tp1', 'tp2', 'tp3']:
+            if values[tp] != '' and values[tp][-1] not in '0123456789':
+                print('Updating')
+                window.Element(tp).Update(values[tp][:-1])
 
     window.Close()
 
