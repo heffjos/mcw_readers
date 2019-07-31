111d068d37d7cf865d9a62717d91867e15e4fa95
Joe Heffernan <jheffheffer@gmail.com>
2019-07-31 17:52:31 -0500

    Added working parsing code to mcw_parse_neuros.py

--- mcw_readers/cli/mcw_parse_neuros.py
+++ mcw_readers/cli/mcw_parse_neuros.py
@@ -1,11 +1,15 @@
 import os
 import sys
+import json
 import openpyxl
 
 import pandas as pd
 import PySimpleGUI as sg
 
 from openpyxl.utils.exceptions import InvalidFileException
+
+from mcw_readers.parsers.clinical import (
+    parse_neuroscore, parse_neuroreader_v2d2d8)
 
 def main():
     layout = [
@@ -27,7 +31,7 @@ def main():
          sg.InputText(key='neuroreader3'),
          sg.FileBrowse()],
         [sg.Text('Neuroscore file', size=(15, 1), auto_size_text=False, justification='right'), 
-         sg.InputText(key='neuroscore_file'), 
+         sg.InputText(key='neuroscore'), 
          sg.FileBrowse()],
         [sg.Submit(), sg.Cancel()]
     ]
@@ -37,17 +41,43 @@ def main():
         event, values = window.Read()
         if event == 'Submit':
             participant = values['participant']
-            runs = values['runs']
-            neuroscore_file = values['neuroscore_file']
-            neuroreader_files = values['neuroreader_files']
+            run_vars = ['tp' + str(x) for x in [1, 2, 3]]
+            neuroreader_vars = ['neuroreader' + str(y) for y in [1, 2, 3]]
+            runs = [values[x] for x in run_vars]
+            neuroreaders = [values[x] for x in neuroreader_vars]
+            neuroscore_file = values['neuroscore']
 
-            sg.Popup('Participant      : {}\n'
-                     'Runs             : {}\n'
-                     'Neuroscore file  : {}\n'
-                     'Neuroreader files: {}\n'.format(participant,
-                                                    runs,
-                                                    neuroscore_file,
-                                                neuroreader_files))
+            # check all neuroreader_files have a run
+            pairs = []
+            for run, neuroreader in zip(runs, neuroreaders):
+                if neuroreader and not run:
+                    raise Exception('Undefined run for neuroreader_file: '
+                                    '{}'.format(pair[1]))
+                if run:
+                    if neuroreader:
+                        pairs.append((run, neuroreader))
+                    else:
+                        pairs.append((run, None))
+
+            # parse now
+            results = []
+            wb = openpyxl.load_workbook(neuroscore_file, read_only=True, data_only=True)
+            for i, pair in enumerate(pairs):
+                date, df = parse_neuroscore(wb, i)
+                if pair[1]:
+                    date, neuroreader_df = parse_neuroreader_v2d2d8(pair[1])
+                    df[neuroreader_df.columns] = neuroreader_df.iloc[0, :].values
+
+                df['date'] = date
+                df['id'] = values['participant']
+                results.append(df)
+            out_file = os.path.splitext(neuroscore_file)[0] + '.csv'
+            pd.concat(results).to_csv(out_file, index=False)
+
+            log_file = os.path.splitext(values['neuroscore'])[0] + '.json'
+            with open(log_file, 'w') as f:
+                json.dump(values, f, indent=4)
+
             break
         if event is None or event == 'Cancel':
             break
