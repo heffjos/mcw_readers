6d837e5e0592d9109066c665d99f5515e4fa58dc
Joe Heffernan <jheffheffer@gmail.com>
2019-08-08 11:42:35 -0500

    Added clinical_update_v3.0latest.R. This file is run if any variables are updated to clinical_unique_spss_variables.tsv. The output csv file is then copy/pasted into a one drive excel file which itself is downloaded into this repository as an xlsx file.

---
+++ mcw_readers/upkeep/clinical_update_v3.0latest.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(readxl)
+
+clinical_unique_spss_variables <- read_delim("../data/clinical_unique_spss_variables.tsv", delim = "\t")
+clinical_v3.0ulatest_labeled <- read_excel("../data/clinical_v3.0ulatest_labeled.xlsx", na = c("", "NA")) %>%
+  select(base, definition, worksheet, row, column)
+
+new_latest <- clinical_unique_spss_variables %>%
+  left_join(clinical_v3.0ulatest_labeled, by = "base") %>%
+  arrange(row, desc(tp), order)
+
+write_csv(new_latest, "./data/new_latest.csv")
