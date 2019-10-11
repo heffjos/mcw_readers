f725a46af70b318487411ac17ca8dfd108a247c8
Joe Heffernan <jheffheffer@gmail.com>
2019-10-10 21:31:22 -0500

    Made recoded variables into a new column instead of the same column.

--- mcw_readers/upkeep/clinical_get_unique_variables.R
+++ mcw_readers/upkeep/clinical_get_unique_variables.R
@@ -3,7 +3,7 @@ library(tidyverse)
 name_mapper = c(
   "filter_$" = "filter_anc",
 
-  # neuropsych_tests
+  # renamed SPSS variables from neuropsych_tests instrument
   "cognitive_change" = "cognitive_change_1",
   "depression_label" = "depression_label_1",
   "depression_label3" = "depression_label_3",
@@ -11,7 +11,7 @@ name_mapper = c(
   "HVLTMemProfile2" = "hvlt_memprofile_2",
   "HVLTMemProfile2_MixedExplained" = "hvlt_memprofile_mixedexplained_2",
   "HVLT_MemProfile_MixedExplained" = "hvlt_memprofile_mixedexplained_1",
-  "letfluency_ver" = "letterfluency_ver_1",
+  "letfluency_ver" = "letfluency_ver_1",
   "trails_ver" = "trails_ver_1",
   "wcst_ver" = "wcst_ver_1")
 
@@ -22,16 +22,16 @@ name_delete = c(
 data <- read_delim("../data/clinical_spss_variables.tsv", delim = "\t")
   
 data <- data %>% 
-  mutate(variable = recode (variable, !!!name_mapper),
-         order = 1:length(variable),
-         tp = str_match(variable, "_([:digit:]|[bB][lL])$")[, 2],
+  mutate(recode_variable = recode(variable, !!!name_mapper),
+         order = 1:length(recode_variable),
+         tp = str_match(recode_variable, "_([:digit:]|[bB][lL])$")[, 2],
          tp = str_to_lower(tp),
          type = case_when(
-            str_detect(variable, "_raw_") ~ "raw",
-            str_detect(variable, "_ss_") ~ "ss",
-            str_detect(variable, "_perc_") ~ "perc",
+            str_detect(recode_variable, "_raw_") ~ "raw",
+            str_detect(recode_variable, "_ss_") ~ "ss",
+            str_detect(recode_variable, "_perc_") ~ "perc",
             TRUE ~ "other"),
-         base = str_remove(variable, "_([:digit:]|[bB][lL])$"),
+         base = str_remove(recode_variable, "_([:digit:]|[bB][lL])$"),
          base = str_to_lower(base),
          tp_fct = fct_lump(tp, 4),
          tp_fct = fct_relevel(tp_fct, "bl", "1", "2", "3", "Other")) %>%
