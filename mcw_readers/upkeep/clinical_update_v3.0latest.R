57f3467887a131c11d8182dd34a90c78dac27669
Joe Heffernan <jheffheffer@gmail.com>
2019-10-10 21:31:51 -0500

    Updated file extension.

--- mcw_readers/upkeep/clinical_update_v3.0latest.R
+++ mcw_readers/upkeep/clinical_update_v3.0latest.R
@@ -9,4 +9,4 @@ new_latest <- clinical_unique_spss_variables %>%
   left_join(clinical_v3.0ulatest_labeled, by = "base") %>%
   arrange(row, desc(tp), order)
 
-write_csv(new_latest, "./data/new_latest.csv")
+write_tsv(new_latest, "./data/new_latest.tsv")
