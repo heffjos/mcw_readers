7c02440385a8aa625206a830948bbcec3bbd562e
Joe Heffernan <jheffheffer@gmail.com>
2020-02-17 16:17:10 -0600

    Removed version functions in utils and renamed a variable to the new naming scheme (aphasia, peds, ecp).

--- mcw_readers/utils.py
+++ mcw_readers/utils.py
@@ -10,25 +10,7 @@ except ImportError:
 
 from . import data
 
-def clinical_build_version_key():
-    version_key = {}
-    with pkg_resources.path(data, 'clinical_version_key.tsv') as key_file:
-        df = pd.read_csv(key_file, sep='\t')
-
-    df['rc'] = list(zip(df['row'], df['column']))
-    versions = set(pd.unique(df['version']))
-    versions.difference({'3.0u2.30', '3.0u11.01.16'})
-    for version in versions:
-        version_df = df[df['version'] == version]
-        version_key[version] = {x: y if not pd.isnull(y) else None 
-                                for x, y in zip(version_df['rc'], 
-                                                version_df['measure'])}
-
-    return version_key
-
-CLINICAL_NEUROSCORE_VERSIONS_IDENTIFIERS = clinical_build_version_key()
-
-CLINICAL_NEUROREADER_MAPPER = {
+APHASIA_NEUROREADER_MAPPER = {
     'WholeBrainMatter': 'WBV',
     'GrayMatter': 'GrayMatter',
     'WhiteMatter': 'WhiteMatter',
@@ -97,44 +79,4 @@ def pdftotext(in_pdf, out_text, options=None):
     args.extend([in_pdf, out_text])
         
     subprocess.run(args)
-
-def clinical_detect_neuroscore_version(wb):
-    """
-    Detects the clinical neuroscore verion.
-
-    **Parameters**
-
-        wb
-            an openpyxl workbook, speed is grealy improved when read_oly=True
-            and maybe data_only=True
-
-    **Outputs**
-
-        version
-            the neuroscore verion. The possible outputs are keys in 
-            CLINICAL_NEUROSCORE_VERSIONS_INDETIFIERS but repeated here:
-                2
-                3.0
-                3.0u1.30
-                3.0u10.21.16
-                3.0u11.01.16
-                3.0u2.3
-                3.0ulatest
-                Old
-            None is returned if the version cannot be identified.
-    """
-    sheet = wb['Template']
-    for version, lookup in CLINICAL_NEUROSCORE_VERSIONS_IDENTIFIERS.items():
-        is_version = True
-        for rc, value in lookup.items():
-            cell_value = sheet.cell(row=rc[0], column=rc[1]).value
-            if cell_value:
-                cell_value = cell_value.strip()
-            if cell_value != value:
-                is_version = False
-                break
-        if is_version:
-            return version
-
-    return None
 
