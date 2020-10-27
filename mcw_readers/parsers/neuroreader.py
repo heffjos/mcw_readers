cc82015994427a5cfccda01f4919d0f3b954266b
Joe Heffernan <jheffheffer@gmail.com>
2020-10-26 21:52:56 -0500

    Removed commented and unsued code. Corrected typo.

--- mcw_readers/parsers/neuroreader.py
+++ mcw_readers/parsers/neuroreader.py
@@ -6,124 +6,7 @@ import PyPDF4 as pdf
 
 from datetime import datetime
 
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    import importlib_resources as pkg_resources
-
-# from .. import data
 from ..utils import NEUROREADER_MAPPERS
-
-# with pkg_resources.path(data, 'clinical_redcap_labeled.tsv') as data_file:
-#     CLINICAL_VARIABLES = pd.read_csv(data_file, sep='\t')
-# 
-# with pkg_resources.path(data, 'clinical_neuropsych_tests.tsv') as data_file:
-#     CLINICAL_TESTS = pd.read_csv(data_file, sep='\t')
-# UNIQUE_TESTS = list(pd.unique(CLINICAL_TESTS['test']))
-# 
-# DATE_COL = 5
-# 
-# def parse_neuroscore(wb, exam, debug=False):
-#     """
-#     Parses neuroscore workbook, primarily the Template worksheet.
-# 
-#     **Parameters**
-# 
-#         wb
-#             an openpyxl workbook, speed is greatly improved when 
-#             read_only=True and maybe data_only=True
-#         exam
-#             The template worksheet has up to 3 visits (known as exames in the)
-#             worksheet) data sets. This controls which exam is parsed. 
-#             It is ***zero-based*** indexing.
-#         debug
-#             This controls whether to parse the exam date. It allows for testing
-#             without setting the date.
-# 
-#     **Outputs**
-#         results
-#             A dataframe. The columns are the measured variables. The rows are
-#             participants. The presence of subtests are added to match the 
-#             redcap import tool csv file.
-#     """
-#     col_adj = exam * 4
-#     version = clinical_detect_neuroscore_version(wb)
-#     if version == None:
-#         raise Exception('Could not detect clinical neuroscore version')
-# 
-#     df_key = CLINICAL_VARIABLES[CLINICAL_VARIABLES['version'] == version].copy()
-#     df_key['column'] = df_key['column'] + col_adj
-# 
-#     worksheets = pd.unique(df_key['worksheet'])
-#     if any(pd.isnull(worksheets)):
-#         raise Exception('There are unassigned worksheets in {}'.format(data_file))
-# 
-#     results = {'variable': [], 'value': []}
-#     # missing assigments have no group value
-#     for ws, df in df_key.groupby('worksheet'):
-#         sheet = wb[ws]
-# 
-#         values = [sheet.cell(row=int(x), column=int(y)).value
-#                   for x, y in zip(df['row'], df['column'])]
-# 
-#         results['variable'].extend(list(df['redcap']))
-#         results['value'].extend(values)
-# 
-#     results['variable'].append('np_date')
-#     if debug:
-#         results['value'].append('07071977')
-#     else:
-#         results['value'].append(wb['Template']
-#                                 .cell(row=9, column=DATE_COL + col_adj)
-#                                 .value
-#                                 .strftime('%Y-%m-%d'))
-# 
-#     results = pd.DataFrame(results).fillna(pd.np.nan)
-# 
-#     garbage = results['value'].str.match(r'^=|^raw$|^val$|^\[ERR\]$|^SS$', 
-#                                          na=False)
-#     results['value'][garbage] = pd.np.nan
-# 
-#     tests_check = CLINICAL_TESTS.merge(results, 
-#                                        how='left', 
-#                                        left_on='redcap', 
-#                                        right_on='variable')
-#     tests_check['check'] = tests_check['value'].notna()
-#     tests_check = (tests_check
-#                    .groupby('test')
-#                    .agg({'check': pd.np.sum})
-#                    .reset_index())
-#     tests_check.rename(columns={'test': 'variable', 'check': 'value'}, inplace=True)
-#     tests_check['value'][tests_check['value'] > 0] = 1
-# 
-#     results = pd.concat((results, tests_check))
-# 
-#     results['index'] = 0
-#     results = results.pivot(index='index', columns='variable', values='value')
-# 
-#     # convert results here
-#     results['gds_class'] = results['gds_class'].map({'WNL': 0,
-#                                                      'Mild': 1,
-#                                                      'Mod': 2,
-#                                                      'Sev': 3})
-#     results['hvlt_delay_ss'] = results['hvlt_delay_ss'].str.replace('^<', '')
-#     results['hvlt_disc_ss'] = results['hvlt_delay_ss'].str.replace('^<', '')
-#     results['hvlt_perc_ss'] = results['hvlt_perc_ss'].str.replace('^<', '')
-#     results['trailsa_err'] = results['trailsa_err'].str.replace('-E$', '')
-#     results['trailsb_err'] = results['trailsb_err'].str.replace('-E$', '')
-#     results['wcst_cat_perc'] = results['wcst_cat_perc'].map({'[<1]': 0,
-#                                                              '[2-5]': 1,
-#                                                              '[6-10]': 2,
-#                                                              '[11-16]': 3,
-#                                                              '[>16]': 4})
-#     results['wcst_fms_perc'] = results['wcst_fms_perc'].map({'[<1]': 0,
-#                                                              '[2-5]': 1,
-#                                                              '[6-10]': 2,
-#                                                              '[11-16]': 3,
-#                                                              '[>16]': 4})
-#     results['wms_io_per'] = results['wms_io_per'].str.extract('^\[(\d+)\]')
-#         
-#     return results
 
 def get_field_locs(working_data, fields):
     """
@@ -152,8 +35,7 @@ def get_field_locs(working_data, fields):
 
     return field_locs
         
-
-def parser_neuroreader(pdf_file, study):
+def parse_neuroreader(pdf_file, study):
     """
     Parses neuroreader files.
 
