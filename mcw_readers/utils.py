5271f32a667913f0b9c683ca598a5bc0b7d1624b
Joe Heffernan <jheffheffer@gmail.com>
2022-01-18 00:37:21 -0600

    Added more utils. The biggest being the psychometric conversion table as a dict and data frame. This lists the standard score and the equivalent values for different metrics.

--- mcw_readers/utils.py
+++ mcw_readers/utils.py
@@ -1,3 +1,56 @@
+import pandas as pd
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    import importlib_resources as pkg_resources
+
+from mcw_readers import data
+
+def get_psychometric_objects():
+    """converts the psychometric conversion table to a dict"""
+
+    fname = 'psychometric_conversion_table_filled.csv'
+
+    with pkg_resources.path(data, fname) as data_file:
+        df = pd.read_csv(data_file.as_posix())
+
+    results = {
+        'standard_score': {},
+        'scaled_score': {},
+        'ets_score': {},
+        't_score': {},
+        'z_score': {},
+        'description': {},
+    }
+    
+    seen = set()
+    for row in df.itertuples():
+        pr = row.percentile_rank
+    
+        if pr in seen:
+            results['standard_score'][pr].append(row.standard_score)
+            results['scaled_score'][pr].append(row.scaled_score)
+            results['ets_score'][pr].append(row.ets_score)
+            results['t_score'][pr].append(row.t_score)
+            results['z_score'][pr].append(row.z_score)
+            results['description'][pr].append(row.description)
+        else:
+            results['standard_score'][pr] = [row.standard_score]
+            results['scaled_score'][pr] = [row.scaled_score]
+            results['ets_score'][pr] = [row.ets_score]
+            results['t_score'][pr] = [row.t_score]
+            results['z_score'][pr] = [row.z_score]
+            results['description'][pr] = [row.description]
+    
+        seen.add(pr)
+
+    return df, results
+
+def close_any(value, test_values, close):
+    """determines if any values in test_values are under close from value"""
+
+    return any([abs(value - x) < close for x in test_values])
 
 NEUROREADER_ECP_HEADER_MAPPER = {
     'Image ID': 'image_id',
@@ -140,3 +193,4 @@ NEUROREADER_MAPPERS = {
                  NEUROREADER_DEMENTIA_TABLE_MAPPER),
 }
 
+DF_PSYCHOMETRIC, DICT_PSYCHOMETRIC = get_psychometric_objects()
